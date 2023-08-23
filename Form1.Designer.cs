459b5c2d1526e240819e5755a2052ec7d230c115
Vitor Gabriel <102386982+vitingr@users.noreply.github.com>
2023-08-22 22:14:52 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.number1 = new System.Windows.Forms.TextBox();
+            this.number2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(308, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome Completo";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // nameInput
+            // 
+            this.nameInput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nameInput.Location = new System.Drawing.Point(149, 108);
+            this.nameInput.Margin = new System.Windows.Forms.Padding(10);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(539, 26);
+            this.nameInput.TabIndex = 2;
+            this.nameInput.TextChanged += new System.EventHandler(this.NameInput_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(535, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 144);
+            this.label3.MinimumSize = new System.Drawing.Size(500, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(500, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ".";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(145, 247);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(100, 26);
+            this.number1.TabIndex = 5;
+            this.number1.TextChanged += new System.EventHandler(this.Number1_TextChanged);
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(346, 247);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(100, 26);
+            this.number2.TabIndex = 6;
+            this.number2.TextChanged += new System.EventHandler(this.Number2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Digite o 1° valor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(342, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Digite o 2° valor";
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(145, 291);
+            this.resultado.MinimumSize = new System.Drawing.Size(500, 0);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(500, 20);
+            this.resultado.TabIndex = 9;
+            this.resultado.Text = ".";
+            this.resultado.Click += new System.EventHandler(this.Resultado_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "Soma",
+            "Subtracao",
+            "Multiplicacao",
+            "Divisao"});
+            this.listBox1.Location = new System.Drawing.Point(568, 221);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox number1;
+        private System.Windows.Forms.TextBox number2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string texto = nameInput.Text;
+            label3.Text = "O seu nome completo é " + texto;
+
+            int.Parse(number1.Text);
+
+            float n1 = float.Parse(number1.Text);
+            float n2 = float.Parse(number2.Text);
+
+            var opcao = listBox1.SelectedItem;
+
+            switch (opcao)
+            {
+                case "Soma":
+                    {
+                        float result = Soma(n1, n2);
+                        resultado.Text = "O resultado da soma entre " + n1 + " e " + n2 + " equivale a " + result;
+                        break;
+                    }
+                case "Subtracao":
+                    {
+                        float result = Subtracao(n1, n2);
+                        resultado.Text = "O resultado da subtração entre " + n1 + " e " + n2 + " equivale a " + result;
+                        break;
+                    }
+                case "Multiplicacao":
+                    {
+                        float result = Multiplicacao(n1, n2);
+                        resultado.Text = "O resultado da multiplicação entre " + n1 + " e " + n2 + " equivale a " + result;
+                        break;
+                    }
+                case "Divisao":
+                    {
+                        float result = Divisao(n1, n2);
+                        resultado.Text = "O resultado da divisão entre " + n1 + " e " + n2 + " equivale a " + result;
+                        break;
+                    }
+                default:
+                    {
+                        resultado.Text = "Erro! Selecione uma opção válida de cálculo";
+                        break;
+                    }
+            }
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Resultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static float Soma(float valor1, float valor2)
+        {
+            float valor = valor1 + valor2;
+            return valor;
+        }
+
+        public static float Subtracao(float valor1, float valor2)
+        {
+            float valor = valor1 - valor2;
+            return valor;
+        }
+
+        public static float Multiplicacao(float valor1, float valor2)
+        {
+            float valor = valor1 * valor2;
+            return valor;
+        }
+
+        public static float Divisao(float valor1, float valor2)
+        {
+            float valor = valor1 / valor2;
+            return valor;
+        }
+    }
+}
